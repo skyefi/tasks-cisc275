683933edb9339571e8baa0fa424314af4fff638e
skyefi <nickyreigel@gmail.com>
2023-10-06 13:46:04 -0400

    finished ChangeColor i think

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,62 @@
 import React, { useState } from "react";
+import { Form, FormCheck } from "react-bootstrap";
+
+const colors: string[] = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink",
+    "brown"
+];
+
+interface makeColorButtonsProps {
+    activeColor: string;
+    setColor: (color: string) => void;
+}
+
+export function makeColorButtons(
+    activeColor: string,
+    setColor: (color: string) => void
+): JSX.Element {
+    function mapColorButtons(color: string): JSX.Element {
+        return (
+            <span>
+                <Form.Check
+                    inline
+                    type="radio"
+                    name="colorSelect"
+                    onChange={() => setColor(color)}
+                    id={"color-check-" + color}
+                    key={"color-check-" + color}
+                    label={color}
+                    style={{ backgroundColor: color }}
+                    value={color}
+                    checked={activeColor === color}
+                    color={color}
+                />
+            </span>
+        );
+    }
+
+    return <div>{colors.map(mapColorButtons)}</div>;
+}
 
 export function ChangeColor(): JSX.Element {
+    const [activeColor, setActiveColor] = useState<string>("");
+
     return (
         <div>
-            <h3>Change Color</h3>
+            {makeColorButtons(activeColor, setActiveColor)}
+            You have chosen{" "}
+            <text
+                data-testid="colored-box"
+                style={{ backgroundColor: activeColor }}
+            >
+                {activeColor === "" ? "nothing yet" : activeColor}
+            </text>
         </div>
     );
 }
